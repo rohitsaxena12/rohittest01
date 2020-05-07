8a81aeace19fdb9c23f5dc0b504f53c7d2c2ed73
RSAX <RSAX@SAG-8C0PMC2.eur.ad.sag>
2020-05-07 17:32:49 +0530

    tempBranch01 Commit: Initial File

---
+++ github-sample-01/src/com/sag/main/SampleJava03.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.sag.main;
+
+/**
+ * @author RSAX
+ *
+ */
+public class SampleJava03 {
+
+	/**
+	 * 
+	 */
+	public SampleJava03() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git Hub Sample Test 02");
+	}
+
+}
