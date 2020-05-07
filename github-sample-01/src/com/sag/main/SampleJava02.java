9ce973f1e9c89bfdc6a3a0cfd2c7f3355b44f780
RSAX <RSAX@SAG-8C0PMC2.eur.ad.sag>
2020-05-07 14:30:49 +0530

    Local Commit. New File Added SampleJava02

---
+++ github-sample-01/src/com/sag/main/SampleJava02.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.sag.main;
+
+/**
+ * @author RSAX
+ *
+ */
+public class SampleJava02 {
+
+	/**
+	 * 
+	 */
+	public SampleJava02() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git Hub Sample Test 02");
+	}
+
+}
